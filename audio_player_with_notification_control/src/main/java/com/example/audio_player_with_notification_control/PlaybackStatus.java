8ff16880f24d2cbf93d9f242acbd80e098db1105
Sargis Khlopuzyan <sargiskh@SARGISKH-394>
2017-11-24 12:53:23 +0400

    Audio Player With Notification Control

---
+++ audio_player_with_notification_control/src/main/java/com/example/audio_player_with_notification_control/PlaybackStatus.java
@@ -0,0 +1,10 @@
+package com.example.audio_player_with_notification_control;
+
+/**
+ * Created by sargiskh on 11/23/2017.
+ */
+
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
---
+++ audio_player_with_notification_control/src/main/java/com/example/audio_player_with_notification_control/onItemClickListener.java
@@ -0,0 +1,11 @@
+package com.example.audio_player_with_notification_control;
+
+import android.view.View;
+
+/**
+ * Created by sargiskh on 11/23/2017.
+ */
+
+public interface onItemClickListener {
+    public void onClick(View view, int index);
+}
